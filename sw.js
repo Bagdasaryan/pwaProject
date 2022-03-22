be7d081457227dce953d26fb794d29bdc3aae595
Mamikon Bagdasaryan <mamikonbagdasaryan@mail.ru>
2022-03-22 23:33:55 +0500

    [PWA][UPD] New test project

--- sw.js
+++ sw.js
@@ -1,26 +1,21 @@
-var cacheName = 'hello-pwa';
-var filesToCache = [
-  '/',
-  '/index.html',
-  '/css/style.css',
-  '/js/main.js'
-];
-
-/* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
+self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
-    })
+    caches.open('fox-store').then((cache) => cache.addAll([
+      '/pwa-examples/a2hs/',
+      '/pwa-examples/a2hs/index.html',
+      '/pwa-examples/a2hs/index.js',
+      '/pwa-examples/a2hs/style.css',
+      '/pwa-examples/a2hs/images/fox1.jpg',
+      '/pwa-examples/a2hs/images/fox2.jpg',
+      '/pwa-examples/a2hs/images/fox3.jpg',
+      '/pwa-examples/a2hs/images/fox4.jpg',
+    ])),
   );
-  self.skipWaiting();
 });
 
-/* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch', (e) => {
+  console.log(e.request.url);
   e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
+    caches.match(e.request).then((response) => response || fetch(e.request)),
   );
 });
